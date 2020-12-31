3cdb844050e6bff7e2cb20f743637e36e653c29a
Vítor Holtz <vitorholtz27@gmail.com>
2020-12-31 20:34:26 -0300

    notification

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,7 @@
 
 let sidebar = document.querySelector('.side-bar')
 let main = document.querySelector('.main')
+let bell = document.querySelector('.notification')
 
 function Opensidebar() {
 	sidebar.classList.toggle('side-bar-expand')
@@ -13,5 +14,10 @@ function sidebarHover() {
 function sidebarHoverOut() {
     main.style.margin = "0px 0px 0px 60px";
 
+}
+
+function shownot() {
+
+    bell.classList.toggle('notification-open')
 }
 
